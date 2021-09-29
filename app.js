bf92729e8d7e6d777846499a244ca83526ba8742
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-09-29 13:55:43 -0700

    added three different buttons with different colors

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const nameInput = document.getElementById('name');
 const button = document.getElementById('generate');
 const firstName = document.getElementById('first-name');
+const bluebutton = document.getElementById('blue');
+const purplebutton = document.getElementById('purple');
+const blackbutton = document.getElementById('black');
+const header = document.getElementById('header');
+const footer = document.getElementById('footer');
+
+bluebutton.addEventListener('click', ()=>{
+    header.style.backgroundColor = 'blue';
+    footer.style.backgroundColor = 'blue'; 
+  
+
+
+
+});
+
+blackbutton.addEventListener('click', ()=>{
+    header.style.backgroundColor = 'black';
+    footer.style.backgroundColor = 'black';
+});
+
+purplebutton.addEventListener('click', ()=>{
+    header.style.backgroundColor = 'purple';
+    footer.style.backgroundColor = 'purple';
+
+});
+
 
 button.addEventListener('click', ()=>{
   
-  let newName = nameInput.value;
-  firstName.textContent = newName;
+    let newName = nameInput.value;
+    firstName.textContent = newName;
 }); 
+
